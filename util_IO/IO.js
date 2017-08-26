a499ea22282654aa5dbdc2403c566de08f1a73f3
Mihir Patel <mihir.v.patel7@gmail.com>
2017-08-26 15:03:19 -0400

    added subfolder capability

--- util_IO/IO.js
+++ util_IO/IO.js
@@ -1,8 +1,11 @@
+//TODO onehotencoding
+//TODO non-image formats
+//TODO folders
+
 function splitNoFolder() {
+	console.log("splitNoFolder")
 	var folderName = document.getElementById('folder_name').value
 	var splitRate = document.getElementById('split_rate').value			
-	console.log(splitRate)
-	console.log(folderName)
 	var root = "./"+folderName+"/"
 	var labelName = document.getElementById('label_name').value
 	var fileExt = document.getElementById('file_ext').value
@@ -44,7 +47,61 @@ print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 	console.log(keras_imports + "\n" + keras_code)
 }
 
+function splitFolder() {
+	console.log("splitFolder")
+	var folderName = document.getElementById('folder_name').value
+	var splitRate = document.getElementById('split_rate').value			
+	var root = "./"+folderName+"/"
+	var labelName = document.getElementById('label_name').value
+	var fileExt = document.getElementById('file_ext').value
+	keras_imports = //import code
+		`
+import os
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+		`
+	keras_code = //code				
+"root = " + "\"" + root + "\"" + "\n" +
+"label = " + "\"" + labelName + "\"" + "\n" +
+"ext = " + "\"" + fileExt + "\"" + "\n" +
+"test_split_rate = " + splitRate + "\n" +
+		`
+ctr = 0 
+dimen = False
+with open(root+label, 'r') as label_index:
+	for line in label_index:
+		path = os.path.join(root,line.split(" ")[0])
+		_,_,files = next(os.walk(path))
+		ctr+=len(files)
+		if(dimen==False):
+			img = Image.open(os.path.join(path,files[0]))
+			dimen = np.array(img).shape
+
+X = np.zeros(( (ctr,)+ dimen))
+y = np.zeros((ctr))
+print(X.shape,y.shape)
+
+idx = 0
+with open(root+label, 'r') as label_index:
+	for line in label_index:
+		lineinfo = line.split(" ")
+		path = os.path.join(root,lineinfo[0])
+		_,_,files = next(os.walk(path))
+		for filename in files:
+			img = Image.open(os.path.join(path,filename))
+			X[idx] = np.array(img)
+			y[idx] = lineinfo[1]
+			idx+=1
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_split_rate)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+		`
+	console.log(keras_imports + "\n" + keras_code)
+}
+
 function noSplitNoFolder() {
+	console.log("noSplitNoFolder")
 	var trainName = document.getElementById('train_name').value
 	var testName = document.getElementById('test_name').value
 
@@ -107,15 +164,96 @@ print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 	console.log(keras_imports + "\n" + keras_code)
 }
 
+function noSplitFolder() {
+	console.log("noSplitFolder")
+	var trainName = document.getElementById('train_name').value
+	var testName = document.getElementById('test_name').value
+
+	var trainRoot = "./"+trainName+"/"
+	var testRoot = "./"+testName+"/"
+
+	var labelName = document.getElementById('label_name').value
+	var fileExt = document.getElementById('file_ext').value
+	keras_imports = //import code
+		`
+import os
+import numpy as np
+from PIL import Image
+		`
+	keras_code = //code				
+"testRoot = " + "\"" + trainRoot + "\"" + "\n" +
+"trainRoot = " + "\"" + testRoot +  "\"" + "\n" +
+"label = " + "\"" + labelName + "\"" + "\n" +
+"ext = " + "\"" + fileExt + "\"" + "\n" +
+		`
+ctr = 0 
+dimen = False
+with open(trainRoot+label, 'r') as label_index:
+	for line in label_index:
+		path = os.path.join(trainRoot,line.split(" ")[0])
+		_,_,files = next(os.walk(path))
+		ctr+=len(files)
+		if(dimen==False):
+			img = Image.open(os.path.join(path,files[0]))
+			dimen = np.array(img).shape
+
+X_train = np.zeros(( (ctr,)+ dimen))
+y_train = np.zeros((ctr))
+print(X_train.shape,y_train.shape)
+
+idx = 0
+with open(trainRoot+label, 'r') as label_index:
+	for line in label_index:
+		lineinfo = line.split(" ")
+		path = os.path.join(trainRoot,lineinfo[0])
+		_,_,files = next(os.walk(path))
+		for filename in files:
+			img = Image.open(os.path.join(path,filename))
+			X_train[idx] = np.array(img)
+			y_train[idx] = lineinfo[1]
+			idx+=1
+
+ctr = 0 
+dimen = False
+with open(testRoot+label, 'r') as label_index:
+	for line in label_index:
+		path = os.path.join(testRoot,line.split(" ")[0])
+		_,_,files = next(os.walk(path))
+		ctr+=len(files)
+		if(dimen==False):
+			img = Image.open(os.path.join(path,files[0]))
+			dimen = np.array(img).shape
+
+X_test = np.zeros(( (ctr,)+ dimen))
+y_test = np.zeros((ctr))
+print(X_test.shape,y_test.shape)
+
+idx = 0
+with open(testRoot+label, 'r') as label_index:
+	for line in label_index:
+		lineinfo = line.split(" ")
+		path = os.path.join(testRoot,lineinfo[0])
+		_,_,files = next(os.walk(path))
+		for filename in files:
+			img = Image.open(os.path.join(path,filename))
+			X_test[idx] = np.array(img)
+			y_test[idx] = lineinfo[1]
+			idx+=1
+
+print(X_train.shape,X_test.shape,y_train.shape,y_test.shape)
+		`
+	console.log(keras_imports + "\n" + keras_code)
+}
+
 function loadData() {
 	var splitData = document.getElementById('split_data').value
 	var useFolder = document.getElementById('use_folder').value
 	if(splitData=="true" && useFolder=="true")
-		splitNoFolder()
-	else if(splitData=="true")
 		splitFolder()
+	else if(splitData=="true")
+		splitNoFolder()
 	else if(useFolder=="true")
-		noSplitFolder
+		noSplitFolder()
 	else
 		noSplitNoFolder()
 }
